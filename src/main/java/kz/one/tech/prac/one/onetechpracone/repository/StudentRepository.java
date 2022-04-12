25a99e710f91eb6625f3875a477ce9de2a190a26
Yerbolat <twinspazyl@gmail.com>
2022-04-12 14:43:09 +0600

    done hw-1

--- src/main/java/kz/one/tech/prac/one/onetechpracone/repository/StudentRepository.java
+++ src/main/java/kz/one/tech/prac/one/onetechpracone/repository/StudentRepository.java
@@ -1,11 +1,9 @@
 package kz.one.tech.prac.one.onetechpracone.repository;
 
 import kz.one.tech.prac.one.onetechpracone.model.Student;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
 public interface StudentRepository {
     List<Student> getAllStudent();
     List<Student> findStudentsByFirstNameAndLastName(String name);
